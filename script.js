3962d0544f218acb7b627c8419fa030df6979b8e
edilson <141554522+Edyylson@users.noreply.github.com>
2023-09-26 19:47:17 -0300

    salvar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var teste = ("teste");
+//var nome = prompt("digite o seu nome!");
+//alert(nome);
 
-alert(teste);
+var num1 = Number(prompt("digite um número de 0 a 100!"));
+var num2 = Number(prompt("digite um número de 0 a 50!"));
+var soma = num1 + num2;
+alert("A soma de todos os numeros é" + " " + soma);
+
