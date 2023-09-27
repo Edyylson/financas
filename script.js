d78ba224c0450a82a175a77d32ec39166e541527
edilson <141554522+Edyylson@users.noreply.github.com>
2023-09-27 05:49:57 -0300

    salvar

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 //var nome = prompt("digite o seu nome!");
 //alert(nome);
 
-var num1 = Number(prompt("digite um número de 0 a 100!"));
-var num2 = Number(prompt("digite um número de 0 a 50!"));
-var soma = num1 + num2;
-alert("A soma de todos os numeros é" + " " + soma);
+//var num1 = Number(prompt("digite um número de 0 a 100!"));
+//var num2 = Number(prompt("digite um número de 0 a 50!"));
+//var soma = num1 + num2;
+//alert("A soma de todos os numeros é" + " " + soma);
+
+//var x = 10;
+//x = x + x / 2;
+//alert("O resultado final de x é" + " " + x);
+
+//var x = prompt("digite um número...");
+//if (x > 10)
+//  alert("aprovado")
+//else
+//  alert("reprovado");
+
+
+//var y = 20;
+//var z = 30;
+//var result = y < z && z / 2 == 15;   o && sig e
+//alert(result);
+
+var tarefas = prompt("você desfilou ou fez o trabalho?")
+if (tarefas == "desfilei" || tarefas == "trabalho")
+  alert("você foi aprovado!")
+else
+  alert("você esta reprovado!");
+
+
+
 
